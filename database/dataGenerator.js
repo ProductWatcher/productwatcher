8f4341e9e8626d314a179b4e4eb85df4d2ba3a4f
eclectictaco <calvin5210@gmail.com>
2020-08-09 22:42:02 -0700

    logic to return dates based on week

--- database/dataGenerator.js
+++ database/dataGenerator.js
@@ -45,6 +45,56 @@ drain and write
 
 */
 
+/* 
+func to genreate dates based on week increments
+takes week
+
+year stays at 2020
+month starts at 08
+day stats at 01
+
+if week is 1
+  return defaults
+
+if week is 2
+  day is 12 
+
+....
+
+if week is 6
+  month is 09
+  day is 5
+
+if week is 7
+  month is 09
+  day is 12
+
+if week is 8
+month is 09
+day is 19
+
+if week is 9
+month is 09
+day is 26
+
+if week is 10
+month is 10
+day is 3
+*/
+
+const dateGenerator = (week) => {
+  if (week === 1) return '2020-08-01';
+  if (week === 2) return '2020-08-08';
+  if (week === 3) return '2020-08-15';
+  if (week === 4) return '2020-08-22';
+  if (week === 5) return '2020-08-29';
+  if (week === 6) return '2020-09-05';
+  if (week === 7) return '2020-09-12';
+  if (week === 8) return '2020-09-19';
+  if (week === 9) return '2020-09-26';
+  if (week === 10) return '2020-10-3';
+}
+
 const writeFiveHundredThousand = (productWriter, priceWriter, encoding, callback) => {
   function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
@@ -67,6 +117,9 @@ const writeFiveHundredThousand = (productWriter, priceWriter, encoding, callback
       const link_to = targetURL;
       const img =  faker.image.imageUrl();
       const productData = `${product_id},${product_name},${product_url},${link_to},${link_to},${img}\n`;
+      let week = 1;
+      const date_of = '' //psql default is yyy-mm-dd
+      const priceData = ``
 
       if (i === 0) {
         productWriter.write(productData, encoding, callback);

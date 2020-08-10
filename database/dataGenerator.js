ab5a4bd86a0aadd79b7f03cece426a7dbbae0670
eclectictaco <calvin5210@gmail.com>
2020-08-10 16:21:06 -0700

    works with smaller data set: still need to fix decimal places

--- database/dataGenerator.js
+++ database/dataGenerator.js
@@ -1,81 +1,9 @@
 const fs = require('fs');
 const faker = require('faker');
-// const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
-// const csvWriter = createCsvWriter({
-//   path: '../CSV/products.csv',
-//   header: [
-//     {id: 'product_id', title:'PRODUCT_ID'},
-//     {id: 'product_name', title: 'PRODUCT_NAME'},
-//     {id: 'link_to', title: 'LINK_TO'},
-//     {id: 'img', title: 'IMG'}
-//   ]
-// });
+require('events').EventEmitter.prototype._maxListeners = 1000;
+require('events').defaultMaxListeners = 1000;
 
-// const records = [
-//   {name: 'Bob',  lang: 'French, English'},
-//   {name: 'Mary', lang: 'English'}
-// ];
-
-// csvWriter.writeRecords(records)       // returns a promise
-// .then(() => {
-//     console.log('...Done');
-// });
-
-/* 
-productWriter
-
-decrement i
-set varaibles and generate fake data
-
-for every one product
-  generate 10 price points with a given ID
-
-if i is zero
-  write to file
-
-otherwise
-drain and write
-
-
-*/
-
-/* 
-func to genreate dates based on week increments
-takes week
-
-year stays at 2020
-month starts at 08
-day stats at 01
-
-if week is 1
-  return defaults
-
-if week is 2
-  day is 12 
-
-....
-
-if week is 6
-  month is 09
-  day is 5
-
-if week is 7
-  month is 09
-  day is 12
-
-if week is 8
-month is 09
-day is 19
-
-if week is 9
-month is 09
-day is 26
-
-if week is 10
-month is 10
-day is 3
-*/
 const writeProducts = fs.createWriteStream('../CSV/products.csv');
 writeProducts.write('product_id,product_name,link_to,img\n', 'utf8');
 const writePrices = fs.createWriteStream('../CSV/prices.csv');
@@ -108,7 +36,7 @@ const writeFiveHundredThousand = (productWriter, priceWriter, encoding, callback
     return Math.floor(rand*power) / power;
 };
 
-  let i = 500000;
+  let i = 500;
   let id = 2000
   let week = 0;
   let price = genRand(10,80,2);
@@ -117,7 +45,7 @@ const writeFiveHundredThousand = (productWriter, priceWriter, encoding, callback
     let ok = true;
     do {
       i -= 1;
-      let price10Count = 10;
+      let price10Count = 2;
       const incrementBy = getRandomIntInclusive(1,255);
       const product_id = id += incrementBy;
       const product_name = faker.commerce.productName();
@@ -125,15 +53,15 @@ const writeFiveHundredThousand = (productWriter, priceWriter, encoding, callback
       const targetURL = `https://www.target.com/${product_url}/-/A-${id}`
       const link_to = targetURL;
       const img =  faker.image.imageUrl();
-      const productData = `${product_id},${product_name},${product_url},${link_to},${link_to},${img}\n`;
+      const productData = `${product_id},${product_name},${link_to},${link_to},${img}\n`;
       let priceOk = true;
         do {
             price10Count -= 1;
-            if (week === 11) {
+            if (week === 10) {
               week = 0
               price = genRand(10,80,2);
             }; //reset month to agust for new product and generate new price for new product
-            week += 1; // to do: week logic does not reset properly, first week results in undefined, second week is set as 2020-08-01
+            week += 1; 
             const date_of = dateGenerator(week);
             const id_of = product_id;
             const priceChange = getRandomIntInclusive(1,5);
@@ -147,7 +75,7 @@ const writeFiveHundredThousand = (productWriter, priceWriter, encoding, callback
             if (price10Count === 0) {
               priceWriter.write(priceData, encoding);
             } else {
-              priceOk = priceWriter.write(priceData);
+              priceOk = priceWriter.write(priceData, encoding);
             }
         } while (price10Count > 0 && priceOk);
         if (price10Count > 0) {

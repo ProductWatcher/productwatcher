1252e5c2158187cf2c0e5c43ef14e0aa2efbd33e
eclectictaco <calvin5210@gmail.com>
2020-08-13 16:20:10 -0700

    loads CSV data

--- database/schema.sql
+++ database/schema.sql
@@ -10,8 +10,8 @@ DROP TABLE IF EXISTS products;
 CREATE TABLE products (
   product_id INTEGER PRIMARY KEY,
   product_name varchar(30) NOT NULL,
-  link_to varchar(30) NOT NULL,
-  img varchar(30)
+  link_to varchar(200) NOT NULL,
+  img varchar(200)
 );
 
 DROP TABLE IF EXISTS price_data;
@@ -23,4 +23,8 @@ CREATE TABLE price_data (
   id_of integer
 );
 
-ALTER TABLE public.price_data ADD CONSTRAINT price_belongs_to FOREIGN KEY (id_of) REFERENCES public.products (product_id)
+ALTER TABLE public.price_data ADD CONSTRAINT price_belongs_to FOREIGN KEY (id_of) REFERENCES public.products (product_id);
+
+\COPY products(product_id, product_name, link_to, img) FROM '../CSV/products.csv' DELIMITER ',' CSV HEADER;
+
+\COPY price_data(date_of, price ,id_of) FROM '../CSV/prices.csv' DELIMITER ',' CSV HEADER;

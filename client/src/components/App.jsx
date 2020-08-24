b8e9da8a59a2e2976e7ff5ec8fd82c095b7c70b2
Phuc Tran <phuctranlv@gmail.com>
2020-08-24 12:05:36 -0700

    Add search result rendering

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import SearchResult from './SearchResult.jsx';
 import ProductModal from './ProductModal.jsx';
 
@@ -6,14 +7,36 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      data: []
     };
+  }
+  componentDidMount () {
+    axios.get('/data')
+      .then(({data}) => {
+        this.setState({data})
+      })
   }
 
   render () {
-    return (
-      <div>hello world</div>
-    )
+    const {data} = this.state;
+    console.log(data)
+    if (data.length > 0) {
+      return (
+        <div>
+          <form>
+            <input type='text' placeholder='Search for product'/>
+            <input type='submit' />
+          </form>
+          <div>
+            {data.map((product) => <SearchResult product={product}/>)}
+          </div>
+        </div>
+      )
+    } else {
+      return (
+        <div>Hello</div>
+      )
+    }
   }
 }
 
--- client/src/components/SearchResult.jsx
+++ client/src/components/SearchResult.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 
-const SearchResult = (props) => {
-  return (
-    <div>search result rendering</div>
-  )
+class SearchResult extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      product: props.product
+    }
+  }
+  render() {
+    console.log(product)
+    const { product } = this.state;
+    return (
+      <div className='search-result'>
+        <div>
+          <div>{product.product_name}</div>
+          <div>{product.product_id}</div>
+        </div>
+        <div>
+          <img src={product.img}/>
+        </div>
+      </div>
+    )
+  }
 };
 
 export default SearchResult;
